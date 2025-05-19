638e2029b7d7c3debe05d294504c489cfe108f3b
Chukwuebuka <Chukwuebuka.dev@gmail.com>
2025-05-19 15:24:28 +0100

    conditional rendering avoiding duplication

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,26 @@
+const ListGroup = () => {
+  let items = [
+    "Nigeria",
+    "Mexico",
+    "United States",
+    "United kingdom",
+    "Japan",
+    "China",
+  ];
+  items = [];
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li className="list-group-item" key={item}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+export default ListGroup;
