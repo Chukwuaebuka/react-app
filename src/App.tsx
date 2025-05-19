3de648ab027f10ec98f10c07c2c714816eab891c
Chukwuebuka <Chukwuebuka.dev@gmail.com>
2025-05-19 16:10:38 +0100

    pass data via props to components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import ListGroup from "./components/ListGroup";
 
 const App = () => {
+  let items = [
+    "Nigeria",
+    "Mexico",
+    "United States",
+    "United kingdom",
+    "Japan",
+    "China",
+  ];
   return (
     <div>
-      <ListGroup />
+      <ListGroup items={items} heading="Countries" />
     </div>
   );
 };
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,19 +1,16 @@
 import { useState } from "react";
 
-const ListGroup = () => {
-  let items = [
-    "Nigeria",
-    "Mexico",
-    "United States",
-    "United kingdom",
-    "Japan",
-    "China",
-  ];
+interface Props {
+  heading: string;
+  items: string[];
+}
+
+const ListGroup = ({ heading, items }: Props) => {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
