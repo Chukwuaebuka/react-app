037f145ab0eb0565c2cd5f9806d4ae90eaf75c3a
Chukwuebuka <Chukwuebuka.dev@gmail.com>
2025-09-22 14:32:35 +0100

    Form component using ref hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 const App = () => {
   return (
     <>
-      <ExpandableText>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur
-        ipsum, itaque, hic, incidunt velit esse voluptas eaque quam sed
-        dignissimos distinctio? Eaque iste porro incidunt saepe voluptatum
-        mollitia quam soluta officia molestias animi illo ut ex aspernatur,
-        illum repellendus. At deserunt necessitatibus blanditiis! Quia molestiae
-        ut voluptatem mollitia quasi molestias doloremque ab. Aut dicta nam
-        harum unde fugiat reiciendis ea dolores ipsum, voluptates vel inventore
-        delectus quos explicabo sequi nostrum, possimus voluptate alias ullam
-        temporibus tempora blanditiis, consectetur voluptas! Facilis facere
-        voluptatem aut ad, quos aliquid iusto labore excepturi veritatis
-        consequatur officiis illum hic. Provident unde porro alias? Inventore,
-        commodi.
-      </ExpandableText>
+      <Form />
     </>
   );
 };
