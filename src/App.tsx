2d8aaf9c4397b9f8b358c4597a133feae5b46c83
Chukwuebuka <Chukwuebuka.dev@gmail.com>
2025-09-24 15:28:10 +0100

    building expense list

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import Form from "./components/Form";
+import { useState } from "react";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 const App = () => {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+    { id: 5, description: "eee", amount: 10, category: "Utilities" },
+  ]);
+
+  const handleDelete = (id: number) => {
+    setExpenses(expenses.filter((exp) => exp.id !== id));
+    console.log("Deleted", id);
+  };
+
   return (
     <>
-      <Form />
+      <ExpenseList expenses={expenses} onDelete={handleDelete} />
     </>
   );
 };
